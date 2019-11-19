34cd00436a12b1e9feaf5135ded9c9aa91a4ea65
tlxiong012 <55326274+tlxiong012@users.noreply.github.com>
2019-11-18 21:56:18 -0600

    Initial commit

---
+++ functionDrills.py
@@ -0,0 +1,69 @@
+'''
+@author: amayamunoz
+'''
+
+'''
+For this assignment you should read the task, then below the task do what it asks you to do.
+For tasks with return statements, you can test out if you are correct by 
+calling the function and printing what is returned
+EXAMPLE TASK:
+'''
+#EX) Define a function that adds two numbers and returns the result
+def add_two_numbers(num1, num2):
+    sumOfTwoNumbers = num1 + num2
+    return sumOfTwoNumbers
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+
+#1) Define a function that subtracts the second number from the first number. Return the result.
+def subtract(num2,num1):
+    differenceOfTwoNumbers=num2-num1
+    return differenceOfTwoNumbers
+#2) Define a function that divides a number by 2, multiplies it by 77, and then adds 10000. Return the result.
+def divide(num1):
+    dividend=num1/2*77+10000
+    return dividend
+#3) Define a function that checks if two numbers are equal. If they are equal, return true. If they are not equal, return false.
+def equivalent(num1,num2):
+    equal= num1==num2
+    return equal
+#4) Define a function that takes in two numbers and returns the larger number. If they are the same, it should just return that number.
+def larger(num1,num2):
+    larger_number=num1>num2
+    if larger_number == num1
+    return true
+#5) Define a function that takes in two words and returns a string that contains both words combined.
+def string(five,dollars):
+    string_together=five+dollars
+    return string_together
+#6) Define a function that takes in three numbers. If the first number is equal to the second OR the third number, return true. Else, return false.
+def sum_three_numbers(num1,num2,num3):
+    sumOfNumbers= num1==num2==num3
+#7) Define a function that prints your name.
+def name(leo):
+    name==leo
+#8) Define a function that takes in a string that is the name of a color. If that string is equal to your favorite color, it prints "That's my favorite color!" If it is not, it prints "That is not my favorite color. Try again."
+def colors(red, blue, black, green):
+    if colors==blue, print(thats my favorite color)
+    if colors==[red, black, green], print(that's not my fav color. Try again)
+#9) Define a function that takes in a number. If the number is not equal to zero, the function runs a loop until the number reaches 0. HINT: Within the loop, keep subtracting 1 from the number.
+def number(7):
+    while b>0:
+    print(b)
+    b-=7
+    
+'''YOUR OWN FUNCTION'''
+
+#10) Create your own function that solves any problem you can think of.
+def numbers(num1,num3):
+    num1==1
+    while num1==1
+    print(num1)
+    num+=1
---
+++ functions.py
@@ -0,0 +1,11 @@
+def addition(num1,num2):
+    sumOfTwoNumbers=num1+num2
+    return sumOfTwoNumbers 
+
+sumOfTwoNumbers=addition(2,5)
+
+def sum_three_numbers(num1,num2,num3):
+    sumOfNumbers=num1+num2+num3
+    return sumOfNumbers
+
+sumOfNumbers=sum_three_numbers(5,4,1)
